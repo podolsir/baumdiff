e3920d7cd21cebed38d31e265509048ac2a7732a
Igor Podolskiy <igor.podolskiy@gmx.de>
2020-09-20 05:10:03 +0200

    Fix license classifier.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: MIT",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     setup_requires=['setuptools_scm'],
