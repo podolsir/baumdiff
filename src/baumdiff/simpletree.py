2ed7cd61d4fcb278abdf94254e29b63e5ebbf91c
Igor Podolskiy <igor.podolskiy@gmx.de>
2020-09-20 13:20:09 +0200

    Simplify node class.

--- src/baumdiff/simpletree.py
+++ src/baumdiff/simpletree.py
@@ -1,13 +1,10 @@
-from collections import defaultdict
-from .treeaccess import DefaultTreeAdapter
-
 class Node(object):
-    def __init__(self, id, value, *args):
+    def __init__(self, id, value, *children):
         self.id = id
         self._value = value
         self._parent = None
         self._children = []
-        for i in args:
+        for i in children:
             self.add(i)
 
     def _get_value(self):
@@ -16,35 +13,33 @@ class Node(object):
         self._value = value
     value = property(_get_value, _set_value)
 
-    def _get_children(self):
+    @property
+    def children(self):
         return self._children
-    children = property(_get_children)
 
-    def _get_parent(self):
+    @property
+    def parent(self):
         return self._parent
-    parent = property(_get_parent)
 
     def __getitem__(self, index):
-        return self.children[index]
+        return self._children[index]
 
     def __len__(self):
-        return len(self.children)
+        return len(self._children)
 
     def __iter__(self):
-        return iter(self.children)
-
+        return iter(self._children)
 
     def index(self, kid):
-        return self.children.index(kid)
+        return self._children.index(kid)
 
-    def _get_path(self):        
+    @property
+    def path(self):        
         if self.parent is None:
             return []
         pp = self.parent.path
         pp.append(self.parent.index(self))
         return pp
-
-    path = property(_get_path)
 
     def add(self, t, index = -1):
         t._parent = self
@@ -60,8 +55,6 @@ class Node(object):
 
     def __eq__(self, other):
         return other is not None and self.id == other.id
-    def __ne__(self, other):
-        return not(self == other)
     def __hash__(self):
         return hash(self.id)
     def __repr__(self):
