a8f2c442e09e1cd3ee76d77712782c7f3561d4f6
Igor Podolskiy <igor.podolskiy@gmx.de>
2020-09-20 10:28:53 +0200

    Fix print tree tests.

--- tests/test_diff/test_treeaccess.py
+++ tests/test_diff/test_treeaccess.py
@@ -8,10 +8,10 @@ def test_print_tree():
     t = N("R", "R", SN("A1"), SN("A2"), SN("A3"))
     buf = StringIO()
     treeaccess.print_tree(t, 0, 2, file = buf)
-    buf == "R\n  A1\n  A2\n  A3"
+    assert buf.getvalue() == "R (R)\n  A1 (A1)\n  A2 (A2)\n  A3 (A3)\n"
 
 def test_print_tree_no_indent():
     t = N("R", "R", SN("A1"), SN("A2"), SN("A3"))
     buf = StringIO()
     treeaccess.print_tree(t, 0, 0, file = buf)
-    buf == "R\nA1\nA2\nA3"
+    assert buf.getvalue() == "R (R)\nA1 (A1)\nA2 (A2)\nA3 (A3)\n"
